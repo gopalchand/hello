fee26a543252720c1a4a79c0334b424e045c604f
Gopal Chand <45721890+gopalchand@users.noreply.github.com>
2025-04-18 23:19:01 +0100

    installation error fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="A Hello World C++ extension with CMake and pybind11",
     author="Your Name",
-    packages=["hello"],
+    packages=["hellothere"],
     package_dir={"": "python"},
     python_requires=">=3.6",
 )
